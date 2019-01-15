bfa3a393dd9391bcf48751bc10ec73e20e78067d
Daniel Dolejška <dolejskad@gmail.com>
2019-01-15 23:35:30 +0100

    Přesun regex výrazů do konfiguračního souboru, podpora pro "subatributy"

--- www/log.php
+++ www/log.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../bootstrap.php';
 
 use Nette\Utils\Json;
 
-
 $filepath = "../logs/{$_GET['file']}";
 if (!is_file($filepath))
 	echo Json::encode([], Json::PRETTY);
@@ -12,7 +11,7 @@ if (!is_file($filepath))
 //TODO: Response cache
 //filemtime($filepath);
 $log = file_get_contents($filepath);
-preg_match_all('/(?<time>[0-9\:]{8}) \| DAYZ-SA --> (?<event>[A-Z\_]+):(?<attrs>.+)/', $log, $matches);
+preg_match_all($service->regex->log_entry, $log, $matches);
 
 $data = [
 	'time' => [
@@ -31,20 +30,41 @@ if (count($matches))
 
 foreach (array_keys($matches[0]) as $entry_id)
 {
-	preg_match_all('/(\s(?<keys>[a-z0-9]+)=\"(?<values>.+?)\")/', $matches['attrs'][$entry_id], $attrs);
+	preg_match_all($service->regex->attribute_entry, $matches['attrs'][$entry_id], $attrs);
 
 	$time = $matches['time'][$entry_id];
 	$event = $matches['event'][$entry_id];
 	$attrs = array_combine($attrs['keys'], $attrs['values']);
 
-	if (isset($attrs['position']))
+	foreach ($service->regex->attributes as $key => $regex)
 	{
-		preg_match('<(?<x>[\-0-9.]+), (?<y>[\-0-9.]+), (?<z>[\-0-9.]+)>', $attrs['position'], $position);
-		$attrs['position'] = [
-			'x' => $position['x'],
-			'y' => $position['y'],
-			'z' => $position['z'],
-		];
+		if (isset($attrs[$key]))
+		{
+			preg_match($regex, $attrs[$key], $attr_matches);
+			$attrs[$key] = [];
+			foreach ($attr_matches as $match_key => $match_value)
+				if (!is_numeric($match_key))
+					$attrs[$key][$match_key] = $match_value;
+		}
+	}
+
+	foreach ($attrs as $key => $value)
+	{
+		$key_array = explode('__', $key);
+		if (count($key_array) > 1)
+		{
+			$key_real = array_splice($key_array, count($key_array) - 1)[0];
+			$x = &$attrs;
+			foreach ($key_array as $sub_key)
+			{
+				if (!isset($x[$sub_key]))
+					$x[$sub_key] = [];
+
+				$x = &$x[$sub_key];
+			}
+			$x[$key_real] = $value;
+			unset($attrs[$key]);
+		}
 	}
 
 	$data['events'][] = [
