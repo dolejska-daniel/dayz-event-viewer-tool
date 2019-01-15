a49073b63a16b24241466f7b7c1b1aa3f5d72db9
Daniel Dolejška <dolejskad@gmail.com>
2019-01-15 20:46:30 +0100

    Načítání serverů z konfiguračních souborů

--- www/servers.php
+++ www/servers.php
@@ -2,14 +2,18 @@
 
 require_once __DIR__ . '/../bootstrap.php';
 
+use Nette\Neon\Neon;
+use Nette\Utils\ArrayHash;
 use Nette\Utils\Json;
 
 
-$data = [
-	"DayZ-SA.cz" => [
-		"donor" => "DONOR",
-	],
-];
+$data = [];
+$serverConfigs = glob(__DIR__ . "/../config/servers/*.neon");
+foreach ($serverConfigs as $config)
+{
+	$config = ArrayHash::from(Neon::decode(file_get_contents($config)));
+	$data[$config->server->group][$config->server->id] = $config->server->name;
+}
 
 
 echo Json::encode($data/*, Json::PRETTY*/);
